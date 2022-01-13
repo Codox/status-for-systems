7799283ea03a1e620ed6669596c17c49c656fb04
Connor Brady <connorbrady@codox.eu>
2022-01-13 08:20:55 +0000

    ADDED Dotenv loading

--- app.js
+++ app.js
@@ -26,7 +26,9 @@
 // > Note: This is not required in order to lift, but it is a convenient default.
 process.chdir(__dirname);
 
-
+// Load environment variables from .env file is specified
+var dotenv = require('dotenv');
+dotenv.config();
 
 // Attempt to import `sails` dependency, as well as `rc` (for loading `.sailsrc` files).
 var sails;
