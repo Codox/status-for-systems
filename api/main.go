caaa572bc1aa2c6691b8c82ea1c0f46c21d39692
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2022-09-30 15:46:04 +0100

    WIP Setting up initial database connections

--- api/main.go
+++ api/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+  "embed"
+  "fmt"
+  "os"
+)
+
+var (
+  //go:embed migrations/*.sql
+  migrations  embed.FS
+)
 
 func main() {
 	fmt.Println("Working123")
+
+  /**
+    Setup database connection
+   */
+  dbConnectionString := fmt.Sprintf(
+    "mysql://user=%s:password=%s@tcp(host=%s:port=%d)/%s?sslmode=disable",
+    os.Getenv("DB_USER"),
+    os.Getenv("DB_PASS"),
+    os.Getenv("DB_HOST"),
+    3306,
+    os.Getenv("DB_NAME"))
+
+  fmt.Println(dbConnectionString)
 }
