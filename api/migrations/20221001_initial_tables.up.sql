e27d82fc8bf182a8a8cc36876838549fe5336f1a
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2022-10-01 11:47:47 +0100

    WIP Initial migrations

---
+++ api/migrations/20221001_initial_tables.up.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS `status_for_systems`.`system_groups` (
+                                                                  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                                                                  `name` VARCHAR(64) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+  ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `status_for_systems`.`systems` (
+                                                            `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                                                            `name` VARCHAR(64) NOT NULL,
+  `group_id` INT UNSIGNED NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE,
+  INDEX `fk_systems_system_groups_id_idx` (`group_id` ASC) VISIBLE,
+  CONSTRAINT `fk_systems_system_groups_id`
+  FOREIGN KEY (`group_id`)
+  REFERENCES `status_for_systems`.`system_groups` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION)
+  ENGINE = InnoDB;
