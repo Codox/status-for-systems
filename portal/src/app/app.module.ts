7ee6c808edc32b1ff1e17e8558aaa2cf36d86ee7
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2022-10-01 11:17:14 +0100

    UPDATED Main file

--- portal/src/app/app.module.ts
+++ portal/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  exports: [
+  ],
+  bootstrap: [DashboardComponent]
 })
 export class AppModule { }
