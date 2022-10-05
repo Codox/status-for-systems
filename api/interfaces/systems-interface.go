3af05d1316605a1f50aa69282316b3a16b6cb7ac
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2022-10-05 07:26:57 +0100

    WIP Setting up models

---
+++ api/interfaces/systems-interface.go
@@ -0,0 +1,19 @@
+package interfaces
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type System struct {
+	gorm.Model
+}
+
+type SystemGroup struct {
+	gorm.Model
+
+	ID        uint64    `gorm:"primaryKey"`
+	Name      string    `gorm:"column:name"`
+	CreatedAt time.Time `gorm:"column:created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at"`
+}
