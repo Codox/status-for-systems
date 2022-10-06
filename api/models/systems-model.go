1326df318ad70146a48e18d245c6b995fa9e8717
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2022-10-06 07:40:08 +0100

    ADDED System models

--- api/models/systems-model.go
+++ api/models/systems-model.go
@@ -7,6 +7,13 @@ import (
 
 type System struct {
 	gorm.Model
+
+	ID          uint64      `gorm:"primaryKey; column:id"`
+	Name        string      `gorm:"column:name"`
+	GroupID     uint64      `gorm:"column:group_id"`
+	SystemGroup SystemGroup `gorm:"foreignKey:GroupID"`
+	CreatedAt   time.Time   `gorm:"column:created_at"`
+	UpdatedAt   time.Time   `gorm:"column:updated_at"`
 }
 
 type SystemGroup struct {
@@ -14,6 +21,7 @@ type SystemGroup struct {
 
 	ID        uint64    `gorm:"primaryKey"`
 	Name      string    `gorm:"column:name"`
+	Systems   []System  `gorm:"foreignKey:GroupID"`
 	CreatedAt time.Time `gorm:"column:created_at"`
 	UpdatedAt time.Time `gorm:"column:updated_at"`
 }
