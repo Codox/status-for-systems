150459a5bcab9c79a6381eb1f4edcca245e660eb
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2022-10-06 08:21:30 +0100

    ADDED UUID Fields to SQL Migrations

--- api/migrations/20221001_initial_tables.up.sql
+++ api/migrations/20221001_initial_tables.up.sql
@@ -1,22 +1,26 @@
 CREATE TABLE IF NOT EXISTS `status_for_systems`.`system_groups` (
-                                                                  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                                                  `name` VARCHAR(64) NOT NULL,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uuid` VARCHAR(36) NOT NULL,
+  `name` VARCHAR(64) NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC)),
+  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC),
   ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `status_for_systems`.`systems` (
-                                                            `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                                            `name` VARCHAR(64) NOT NULL,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(64) NOT NULL,
+  `uuid` VARCHAR(36) NOT NULL,
   `group_id` INT UNSIGNED NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `name_UNIQUE` (`name` ASC),
+  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC),
   INDEX `fk_systems_system_groups_id_idx` (`group_id` ASC),
   CONSTRAINT `fk_systems_system_groups_id`
   FOREIGN KEY (`group_id`)
