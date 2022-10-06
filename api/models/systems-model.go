70d33770df8819bf24d6504472704dfbc92ee56d
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2022-10-06 08:26:45 +0100

    ADDED UUID fields to structs

--- api/models/systems-model.go
+++ api/models/systems-model.go
@@ -1,11 +1,12 @@
 package models
 
 import (
-  "time"
+	"time"
 )
 
 type System struct {
 	ID          uint64      `gorm:"primaryKey; column:id" json:"-"`
+	UUID        string      `gorm:"column:uuid" json:"uuid"`
 	Name        string      `gorm:"column:name" json:"name"`
 	GroupID     uint64      `gorm:"column:group_id" json:"-"`
 	SystemGroup SystemGroup `gorm:"foreignKey:GroupID" json:"systemGroup"`
@@ -15,6 +16,7 @@ type System struct {
 
 type SystemGroup struct {
 	ID        uint64    `gorm:"primaryKey; column:id" json:"-"`
+	UUID      string    `gorm:"column:uuid" json:"uuid"`
 	Name      string    `gorm:"column:name" json:"name"`
 	Systems   []System  `gorm:"foreignKey:GroupID" json:"systems"`
 	CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
