1e3cef6ca565b62b0bacd67538032718021072b1
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2022-11-08 11:42:37 +0000

    WIP Adding migrations

--- api/migrations/20221108_add_fields_for_incidents.up.sql
+++ api/migrations/20221108_add_fields_for_incidents.up.sql
@@ -1,28 +1,29 @@
-CREATE TABLE IF NOT EXISTS `system_groups` (
-                                                                  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                                                  `uuid` VARCHAR(36) NOT NULL,
-  `name` VARCHAR(64) NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NOT NULL DEFAULT TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE,
-  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE)
-  ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS `systems` (
-                                                            `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                                            `uuid` VARCHAR(36) NOT NULL,
-  `name` VARCHAR(64) NOT NULL,
-  `group_id` INT UNSIGNED NOT NULL,
+-- Create lookup table for incident statuses
+CREATE TABLE IF NOT EXISTS `incident_status` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uuid` VARCHAR(36) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE,
-  INDEX `fk_systems_system_groups_id_idx` (`group_id` ASC) VISIBLE,
-  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE,
-  CONSTRAINT `fk_systems_system_groups_id`
-  FOREIGN KEY (`group_id`)
-  REFERENCES `status_for_systems`.`system_groups` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION)
+  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE)
+  UNIQUE INDEX `name_UNIQUE` (`uuid` ASC) VISIBLE)
   ENGINE = InnoDB;
+
+-- Insert static data
+INSERT INTO `incident_status` (`uuid`, `name`) VALUES (UUID(), 'Investigating');
+INSERT INTO `incident_status` (`uuid`, `name`) VALUES (UUID(), 'Identified');
+INSERT INTO `incident_status` (`uuid`, `name`) VALUES (UUID(), 'Monitoring');
+INSERT INTO `incident_status` (`uuid`, `name`) VALUES (UUID(), 'Resolved');
+
+-- Create incidents table
+CREATE TABLE IF NOT EXISTS `incidents` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uuid` VARCHAR(36) NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE)
+  ENGINE = InnoDB;
+
