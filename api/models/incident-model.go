d1c9559f64304ae7530a822b161e2c5865d5df82
Connor Brady <connorbrady@codox.eu>
2022-11-08 15:12:26 +0000

    UPDATED Incident models

--- api/models/incident-model.go
+++ api/models/incident-model.go
@@ -11,23 +11,27 @@ type IncidentStatus struct {
 }
 
 type IncidentUpdate struct {
-	ID        uint64    `gorm:"primaryKey; column:id" json:"-"`
-	UUID      string    `gorm:"column:uuid" json:"uuid"`
-	Text      string    `gorm:"column:text;type:text" json:"text"`
-	CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
-	UpdatedAt time.Time `gorm:"column:updated_at" json:"updatedAt"`
-	DeletedAt time.Time `gorm:"column:deleted_at" json:"deletedAt"`
+	ID               uint64         `gorm:"primaryKey; column:id" json:"-"`
+	UUID             string         `gorm:"column:uuid" json:"uuid"`
+	Text             string         `gorm:"column:text;type:text" json:"text"`
+	IncidentID       uint64         `gorm:"column:incident_id" json:"-"`
+	IncidentStatusID uint64         `gorm:"column:incident_status_id" json:"-"`
+	IncidentStatus   IncidentStatus `gorm:"foreignKey:IncidentStatusID;references:ID" json:"status"`
+	CreatedAt        time.Time      `gorm:"column:created_at" json:"createdAt"`
+	UpdatedAt        time.Time      `gorm:"column:updated_at" json:"updatedAt"`
+	DeletedAt        time.Time      `gorm:"column:deleted_at" json:"deletedAt"`
 }
 
 type Incident struct {
-	ID               uint64         `gorm:"primaryKey; column:id" json:"-"`
-	UUID             string         `gorm:"column:uuid" json:"uuid"`
-	Title            string         `gorm:"column:title" json:"title"`
-	IncidentStatusID uint64         `gorm:"column:incident_status_id" json:"-"`
-	IncidentStatus   IncidentStatus `gorm:"foreignKey:IncidentStatusID;references:ID" json:"status"`
-	Description      string         `gorm:"column:description;type:text" json:"description"`
-	CreatedAt        time.Time      `gorm:"column:created_at" json:"createdAt"`
-	UpdatedAt        time.Time      `gorm:"column:updated_at" json:"updatedAt"`
+	ID               uint64           `gorm:"primaryKey; column:id" json:"-"`
+	UUID             string           `gorm:"column:uuid" json:"uuid"`
+	Title            string           `gorm:"column:title" json:"title"`
+	IncidentStatusID uint64           `gorm:"column:incident_status_id" json:"-"`
+	Status           IncidentStatus   `gorm:"foreignKey:IncidentStatusID;references:ID" json:"status"`
+	Updates          []IncidentUpdate `gorm:"foreignKey:IncidentID;references:ID" json:"updates,omitempty"`
+	Description      string           `gorm:"column:description;type:text" json:"description"`
+	CreatedAt        time.Time        `gorm:"column:created_at" json:"createdAt"`
+	UpdatedAt        time.Time        `gorm:"column:updated_at" json:"updatedAt"`
 }
 
 type IncidentSystem struct {
