322522b56282e61a113b0adde832ccd36a624f8d
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2022-11-08 10:57:58 +0000

    ADDED Model structures for lookup tables

--- api/models/incident-model.go
+++ api/models/incident-model.go
@@ -3,22 +3,37 @@ package models
 import "time"
 
 type IncidentStatus struct {
-  ID        uint64    `gorm:"primaryKey; column:id" json:"-"`
-  UUID      string    `gorm:"column:uuid" json:"uuid"`
-  Name      string    `gorm:"column:name" json:"name"`
-  CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
-  UpdatedAt time.Time `gorm:"column:updated_at" json:"updatedAt"`
+	ID        uint64    `gorm:"primaryKey; column:id" json:"-"`
+	UUID      string    `gorm:"column:uuid" json:"uuid"`
+	Name      string    `gorm:"column:name" json:"name"`
+	CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
+	UpdatedAt time.Time `gorm:"column:updated_at" json:"updatedAt"`
 }
 
-type IncidentUpdates struct {
-  ID        uint64    `gorm:"primaryKey; column:id" json:"-"`
-  UUID      string    `gorm:"column:uuid" json:"uuid"`
-  Text      string    `gorm:"column:text;type:text" json:"text"`
-  CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
-  UpdatedAt time.Time `gorm:"column:updated_at" json:"updatedAt"`
-  DeletedAt time.Time `gorm:"column:deleted_at" json:"deletedAt"`
+type IncidentUpdate struct {
+	ID        uint64    `gorm:"primaryKey; column:id" json:"-"`
+	UUID      string    `gorm:"column:uuid" json:"uuid"`
+	Text      string    `gorm:"column:text;type:text" json:"text"`
+	CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
+	UpdatedAt time.Time `gorm:"column:updated_at" json:"updatedAt"`
+	DeletedAt time.Time `gorm:"column:deleted_at" json:"deletedAt"`
 }
 
 type Incident struct {
+	ID          uint64    `gorm:"primaryKey; column:id" json:"-"`
+	UUID        string    `gorm:"column:uuid" json:"uuid"`
+	Title       string    `gorm:"column:title" json:"title"`
+	Description string    `gorm:"column:description;type:text" json:"description"`
+	CreatedAt   time.Time `gorm:"column:created_at" json:"createdAt"`
+	UpdatedAt   time.Time `gorm:"column:updated_at" json:"updatedAt"`
+}
 
+type IncidentSystem struct {
+	ID         uint64    `gorm:"primaryKey; column:id" json:"-"`
+	UUID       string    `gorm:"column:uuid" json:"uuid"`
+	SystemID   uint64    `gorm:"column:system_id" json:"-"`
+  SystemStatusID uint64 `gorm:"column:system_status_id" json:"-"`
+	IncidentID uint64    `gorm:"column:incident_id" json:"-"`
+	CreatedAt  time.Time `gorm:"column:created_at" json:"createdAt"`
+	UpdatedAt  time.Time `gorm:"column:updated_at" json:"updatedAt"`
 }
--- api/models/systems-model.go
+++ api/models/systems-model.go
@@ -21,3 +21,11 @@ type SystemGroup struct {
 	CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
 	UpdatedAt time.Time `gorm:"column:updated_at" json:"updatedAt"`
 }
+
+type SystemStatus struct {
+  ID        uint64    `gorm:"primaryKey; column:id" json:"-"`
+  UUID      string    `gorm:"column:uuid" json:"uuid"`
+  Name      string    `gorm:"column:name" json:"name"`
+  CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
+  UpdatedAt time.Time `gorm:"column:updated_at" json:"updatedAt"`
+}
