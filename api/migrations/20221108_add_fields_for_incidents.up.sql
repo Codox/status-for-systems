046c3ca533f3c99de3ce7864ea1e6697cc31a816
Connor Brady <connorbrady@codox.eu>
2022-11-08 12:10:59 +0000

    WIP More on the migrations of  the models

--- api/migrations/20221108_add_fields_for_incidents.up.sql
+++ api/migrations/20221108_add_fields_for_incidents.up.sql
@@ -21,9 +21,13 @@ CREATE TABLE IF NOT EXISTS `incidents` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `uuid` VARCHAR(36) NOT NULL,
   `title` VARCHAR(255) NOT NULL,
+  `incident_status_id` INT UNSIGNED NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NOT NULL DEFAULT TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE)
+  CONSTRAINT `fk_systems_system_groups_id`
+  FOREIGN KEY (`group_id`)
+  REFERENCES `status_for_systems`.`system_groups` (`id`)
   ENGINE = InnoDB;
 
