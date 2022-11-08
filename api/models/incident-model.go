fd355c4bcf66c359335d94428583c48901dee4fe
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2022-11-08 10:41:53 +0000

    WIP Incident models

--- api/models/incident-model.go
+++ api/models/incident-model.go
@@ -10,6 +10,15 @@ type IncidentStatus struct {
   UpdatedAt time.Time `gorm:"column:updated_at" json:"updatedAt"`
 }
 
+type IncidentUpdates struct {
+  ID        uint64    `gorm:"primaryKey; column:id" json:"-"`
+  UUID      string    `gorm:"column:uuid" json:"uuid"`
+  Text      string    `gorm:"column:text;type:text" json:"text"`
+  CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
+  UpdatedAt time.Time `gorm:"column:updated_at" json:"updatedAt"`
+  DeletedAt time.Time `gorm:"column:deleted_at" json:"deletedAt"`
+}
+
 type Incident struct {
 
 }
