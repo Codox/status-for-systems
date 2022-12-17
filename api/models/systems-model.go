9ad93c8f28291a70e87d0fc3aeb73377f919df6f
Connor Brady <connorbrady@codox.eu>
2022-12-17 17:14:39 +0000

    FIXED Few relation issues

--- api/models/systems-model.go
+++ api/models/systems-model.go
@@ -5,19 +5,21 @@ import (
 )
 
 type System struct {
-	ID        uint64       `gorm:"primaryKey; column:id" json:"-"`
-	UUID      string       `gorm:"column:uuid" json:"uuid"`
-	Name      string       `gorm:"column:name" json:"name"`
-	Status    SystemStatus `gorm:"foreignKey:StatusID;references:ID" json:"status"`
-	GroupID   uint64       `gorm:"column:group_id" json:"-"`
-	CreatedAt time.Time    `gorm:"column:created_at" json:"createdAt"`
-	UpdatedAt time.Time    `gorm:"column:updated_at" json:"updatedAt"`
+	ID             uint64       `gorm:"primaryKey; column:id" json:"-"`
+	UUID           string       `gorm:"column:uuid" json:"uuid"`
+	Name           string       `gorm:"column:name" json:"name"`
+	Status         SystemStatus `gorm:"foreignKey:SystemStatusID;references:ID" json:"status"`
+	GroupID        uint64       `gorm:"column:group_id" json:"-"`
+	SystemStatusID uint64       `gorm:"column:system_status_id" json:"-"`
+	CreatedAt      time.Time    `gorm:"column:created_at" json:"createdAt"`
+	UpdatedAt      time.Time    `gorm:"column:updated_at" json:"updatedAt"`
 }
 
 type SystemGroup struct {
 	ID        uint64    `gorm:"primaryKey; column:id" json:"-"`
 	UUID      string    `gorm:"column:uuid" json:"uuid"`
 	Name      string    `gorm:"column:name" json:"name"`
+	Systems   []System  `gorm:"foreignKey:GroupID;references:ID" json:"systems"`
 	CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
 	UpdatedAt time.Time `gorm:"column:updated_at" json:"updatedAt"`
 }
