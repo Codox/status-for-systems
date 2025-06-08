bf2251ebd745b4114498d37be0e7ad2539b34c39
rexchoppers <connorbrady@codox.uk>
2025-06-08 21:20:38 +0100

    REMOVED Unused files (For now)

--- src/app/api/models/index.ts
+++
@@ -1,4 +0,0 @@
-import { Component } from './Component';
-import { Group } from './Group';
-
-export { Component, Group }; 
--- src/config/dashboard.ts
+++
@@ -1,4 +0,0 @@
-export const dashboardConfig = {
-  title: process.env.NEXT_PUBLIC_DASHBOARD_TITLE || "Status For Systems",
-  description: process.env.NEXT_PUBLIC_DASHBOARD_DESCRIPTION || "Real-time status of all systems and services"
-} as const; 
