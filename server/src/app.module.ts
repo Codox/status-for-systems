6cea6fa039d4517bf37c1f4557970fcfd8648121
rexchoppers <connorbrady@codox.uk>
2025-06-09 18:32:11 +0100

    REMOVED Database configuration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
--- server/src/config/database.config.ts
+++
@@ -1,7 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('database', () => ({
-  uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/status-for-systems',
-  user: process.env.MONGODB_USER,
-  password: process.env.MONGODB_PASSWORD,
-})); 
