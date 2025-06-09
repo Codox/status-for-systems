ac156f7167b55d4198616e4a188ece2af2058ad6
rexchoppers <connorbrady@codox.uk>
2025-06-09 09:46:05 +0100

    ADDED Database configuration for main app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri'),
+        user: configService.get<string>('database.user'),
+        pass: configService.get<string>('database.password'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
