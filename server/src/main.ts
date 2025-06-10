60a1ba13812473dbc934ed790943a6d3443552a8
rexchoppers <connorbrady@codox.uk>
2025-06-10 21:29:45 +0100

    ADDED CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
