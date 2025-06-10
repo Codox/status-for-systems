cb238f6f11946cf0a55704bcdb7e593cda691534
rexchoppers <connorbrady@codox.uk>
2025-06-10 20:53:00 +0100

    UPDATED Basic auth bits

---
+++ server/src/auth/basic-auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class BasicAuthGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new UnauthorizedException('No authorization header');
+    }
+
+    return this.authService.validateBasicAuth(authHeader);
+  }
+} 
