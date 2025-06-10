609211db889208b9b59968d608471dc3ccde8030
rexchoppers <connorbrady@codox.uk>
2025-06-10 18:12:53 +0100

    ADDED Users bits

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,21 +1,19 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-export type UserDocument = User & Document;
-
 @Schema({ timestamps: true })
-export class User {
+export class User extends Document  {
   @Prop({ required: true })
-  username: string;
+  firstName: string;
+  
+  @Prop({ required: true })
+  lastName: string;
 
   @Prop({ required: true, unique: true })
   email: string;
 
   @Prop({ required: true })
   password: string;
-
-  @Prop({ default: false })
-  isAdmin: boolean;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User); 
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,13 +1,13 @@
 import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User, UserDocument } from './entities/user.entity';
+import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(User.name) private userModel: Model<User>,
   ) {}
 
   async create(username: string, email: string, password: string): Promise<User> {
