c569bdc99e7c5e9e047a4e1f435708edcdf6f628
rexchoppers <connorbrady@codox.uk>
2025-06-17 21:01:09 +0100

    UPDATED Groups page

--- web/src/app/admin/groups/page.tsx
+++ web/src/app/admin/groups/page.tsx
@@ -18,8 +18,16 @@ import {
   Skeleton,
   Center,
   Alert,
-  AlertIcon
+  AlertIcon,
+  Card,
+  CardHeader,
+  CardBody,
+  Icon,
+  HStack,
+  VStack,
+  Container
 } from '@chakra-ui/react'
+import { LinkIcon, AddIcon, EditIcon } from '@chakra-ui/icons'
 
 interface Component {
   _id: string;
@@ -70,87 +78,203 @@ export default function GroupsPage() {
 
   if (isLoading) {
     return (
-      <Center minH="50vh">
-        <Skeleton>
-          <Text color={textColor}>Loading groups...</Text>
-        </Skeleton>
-      </Center>
+      <Container maxW="container.xl" p={0}>
+        {/* Header skeleton */}
+        <Flex justify="space-between" align="center" mb={6} mt={4}>
+          <Box>
+            <Skeleton height="2rem" width="12rem" mb={2} />
+            <Skeleton height="1rem" width="20rem" />
+          </Box>
+          <Skeleton height="2rem" width="8rem" />
+        </Flex>
+
+        {/* Card skeleton */}
+        <Card shadow="md" borderRadius="lg" bg={cardBg} overflow="hidden" mb={6}>
+          <CardHeader bg={headerBg} py={3} px={6}>
+            <Flex justify="space-between" align="center">
+              <Skeleton height="1.5rem" width="8rem" />
+              <Skeleton height="1rem" width="5rem" />
+            </Flex>
+          </CardHeader>
+          <CardBody>
+            <VStack spacing={4} align="stretch">
+              {[1, 2, 3].map((i) => (
+                <Box key={i} p={2}>
+                  <Skeleton height="1.5rem" width="60%" mb={2} />
+                  <Skeleton height="1rem" width="90%" mb={2} />
+                  <Skeleton height="1rem" width="30%" />
+                </Box>
+              ))}
+            </VStack>
+          </CardBody>
+        </Card>
+      </Container>
     )
   }
 
   if (error) {
     return (
-      <Alert status="error" borderRadius="md" mt={4}>
-        <AlertIcon />
-        {error}
-      </Alert>
-    )
-  }
-
-  return (
-    <Box spacing={6}>
-      <Flex justify="space-between" align={{ base: "start", sm: "center" }} direction={{ base: "column", sm: "row" }} mb={6}>
-        <Box>
-          <Heading as="h1" size="lg" mb={1}>
-            Groups
-          </Heading>
-          <Text color={textColor}>
-            Manage your service groups and their components
-          </Text>
-        </Box>
-        <Box mt={{ base: 4, sm: 0 }}>
+      <Container maxW="container.xl" p={0}>
+        {/* Header with actions */}
+        <Flex
+          direction={{ base: "column", sm: "row" }}
+          justify="space-between"
+          align={{ base: "flex-start", sm: "center" }}
+          mb={6}
+          mt={4}
+          gap={4}
+        >
+          <Box>
+            <Heading as="h1" size="lg" mb={1}>
+              Service Groups
+            </Heading>
+            <Text color={textColor}>
+              Manage your service groups and their components
+            </Text>
+          </Box>
           <Button
             as={NextLink}
             href="/admin/groups/new"
             colorScheme="blue"
             size="sm"
+            leftIcon={<AddIcon />}
+            alignSelf={{ base: "flex-start", sm: "auto" }}
           >
             Add Group
           </Button>
+        </Flex>
+
+        {/* Error message */}
+        <Alert status="error" borderRadius="lg" mb={6}>
+          <AlertIcon />
+          <Box>
+            <Heading as="h4" size="sm" mb={1}>
+              Error Loading Groups
+            </Heading>
+            <Text fontSize="sm">{error}</Text>
+          </Box>
+        </Alert>
+      </Container>
+    )
+  }
+
+  return (
+    <Container maxW="container.xl" p={0}>
+      {/* Header with actions */}
+      <Flex
+        direction={{ base: "column", sm: "row" }}
+        justify="space-between"
+        align={{ base: "flex-start", sm: "center" }}
+        mb={6}
+        mt={4}
+        gap={4}
+      >
+        <Box>
+          <Heading as="h1" size="lg" mb={1}>
+            Service Groups
+          </Heading>
+          <Text color={textColor}>
+            Manage your service groups and their components
+          </Text>
         </Box>
+        <Button
+          as={NextLink}
+          href="/admin/groups/new"
+          colorScheme="blue"
+          size="sm"
+          leftIcon={<AddIcon />}
+          alignSelf={{ base: "flex-start", sm: "auto" }}
+        >
+          Add Group
+        </Button>
       </Flex>
 
       {/* Groups List */}
-      <Box bg={cardBg} shadow="md" borderRadius="md" overflow="hidden">
-        <List divider={<Divider />}>
+      <Card shadow="md" borderRadius="lg" bg={cardBg} overflow="hidden" mb={6}>
+        <CardHeader bg={headerBg} py={3} px={{ base: 4, md: 6 }}>
+          <Flex
+            direction={{ base: "column", sm: "row" }}
+            justify="space-between"
+            align={{ base: "flex-start", sm: "center" }}
+            gap={{ base: 2, sm: 0 }}
+          >
+            <HStack spacing={2}>
+              <Icon as={LinkIcon} color="blue.500" />
+              <Heading as="h3" size="md">
+                Groups
+              </Heading>
+            </HStack>
+            <Text fontSize="sm" color={textColor}>
+              {groups.length} {groups.length === 1 ? 'group' : 'groups'} total
+            </Text>
+          </Flex>
+        </CardHeader>
+        <CardBody p={0}>
           {groups.length > 0 ? (
-            groups.map((group: Group) => (
-              <ListItem key={group._id}>
-                <Box px={4} py={4}>
-                  <Flex justify="space-between" align="center">
-                    <Flex align="center">
-                      <Text fontWeight="medium" color="blue.600" isTruncated>
-                        {group.name}
-                      </Text>
-                      <Badge ml={2} colorScheme="green" borderRadius="full" px={2}>
-                        {group.components?.length || 0} components
-                      </Badge>
-                    </Flex>
-                    <Link
-                      as={NextLink}
-                      href={`/admin/groups/${group._id}`}
-                      color="blue.600"
-                      fontWeight="medium"
-                      _hover={{ color: "blue.500" }}
+            <List>
+              {groups.map((group: Group) => (
+                <ListItem key={group._id} borderBottomWidth={1} borderColor={borderColor} _last={{ borderBottomWidth: 0 }}>
+                  <Box px={{ base: 4, md: 6 }} py={4} _hover={{ bg: hoverBg }} transition="background 0.2s">
+                    <Flex
+                      direction={{ base: "column", md: "row" }}
+                      justify="space-between"
+                      align="flex-start"
+                      gap={{ base: 3, md: 0 }}
                     >
-                      Edit
-                    </Link>
-                  </Flex>
-                  <Box mt={2}>
-                    <Text fontSize="sm" color={textColor}>
-                      {group.description}
-                    </Text>
+                      <HStack spacing={3} align="flex-start">
+                        <Icon as={LinkIcon} mt={1} color="blue.500" />
+                        <Box>
+                          <Heading as="h4" size="sm" mb={1} color="blue.600">
+                            {group.name}
+                          </Heading>
+                          <Text fontSize="sm" color={textColor} mb={2}>
+                            {group.description}
+                          </Text>
+                          <Badge colorScheme="green" borderRadius="full" px={2}>
+                            {group.components?.length || 0} {group.components?.length === 1 ? 'component' : 'components'}
+                          </Badge>
+                        </Box>
+                      </HStack>
+                      <Button
+                        as={NextLink}
+                        href={`/admin/groups/${group._id}`}
+                        size="sm"
+                        colorScheme="blue"
+                        variant="ghost"
+                        leftIcon={<EditIcon />}
+                        alignSelf={{ base: "flex-start", md: "center" }}
+                        mt={{ base: 0, md: 1 }}
+                      >
+                        Edit
+                      </Button>
+                    </Flex>
                   </Box>
-                </Box>
-              </ListItem>
-            ))
+                </ListItem>
+              ))}
+            </List>
           ) : (
-            <ListItem px={4} py={4} textAlign="center" color={textColor}>
-              No groups found. Create your first group to get started.
-            </ListItem>
+            <Box px={{ base: 4, md: 6 }} py={{ base: 6, md: 8 }} textAlign="center">
+              <VStack spacing={3}>
+                <Icon as={LinkIcon} boxSize="3rem" color="gray.300" />
+                <Text fontWeight="medium">No groups found</Text>
+                <Text color={textColor} fontSize="sm">
+                  Create your first group to get started.
+                </Text>
+                <Button
+                  as={NextLink}
+                  href="/admin/groups/new"
+                  colorScheme="blue"
+                  size="sm"
+                  mt={2}
+                  leftIcon={<AddIcon />}
+                >
+                  Add Group
+                </Button>
+              </VStack>
+            </Box>
           )}
-        </List>
-      </Box>
-    </Box>
+        </CardBody>
+      </Card>
+    </Container>
   )
 }
