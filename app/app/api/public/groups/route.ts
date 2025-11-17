495dd10d60c21a97b2609548ffc9a5b7ae32f609
rexchoppers <rexchoppers@gmail.com>
2025-11-17 20:31:41 +0000

    WIP Adding main file

--- app/app/api/public/groups/route.ts
+++ app/app/api/public/groups/route.ts
@@ -1,3 +1,21 @@
+import dbConnect from '@/lib/mongodb';
+import GroupModel from '@/lib/entities/group.entity';
+import { NextResponse } from 'next/server';
+
 export async function GET() {
-    return Response.json({ message: "Hello API" });
+  try {
+    await dbConnect();
+    
+    const groups = await GroupModel.find()
+        // .populate('components')
+        .exec();
+    
+    return NextResponse.json(groups, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching groups:', error);
+    return NextResponse.json(
+      { error: 'Failed to fetch groups' },
+      { status: 500 }
+    );
+  }
 }
